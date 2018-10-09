044b7f39c390a6e00dec5fd69ca9692d3c9ff728
Maximilien Delangle <40693136+Elkawaie@users.noreply.github.com>
2018-10-09 15:58:39 +0200

    Correction erreur
    Petite erreur a la création d'un évenement une donnée était manquante

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Toiba\FullCalendarBundle\FullCalendarBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class =>['all' => true],
+    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
 ];
